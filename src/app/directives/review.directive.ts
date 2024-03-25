730b498652aaf4f519c5abc388ab26f3ec2bf4e1
vgri <vgri199712@163.com>
2024-03-25 11:27:46 +0800

    ngTemplateOutlet学习

--- src/app/directives/review.directive.ts
+++ src/app/directives/review.directive.ts
@@ -1,31 +1,8 @@
-import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
-
-export class VgriUnlessContext<T = unknown> {
-  $implicit: T = null!;
-  appReview: T = null!;
-  name: T = null!;
-}
+import { Directive } from '@angular/core';
 
 @Directive({
   selector: '[appReview]',
 })
 export class ReviewDirective {
-  hasView = false;
-  _context = new VgriUnlessContext();
-  @Input() set appReview(condition: boolean) {
-    if (!condition && !this.hasView) {
-      this._context.$implicit = this._context.appReview = condition;
-      this._context.name = 'vgri is good';
-      this.viewContainerRef.createEmbeddedView(this.templateRef, this._context);
-      this.hasView = true;
-    } else if (this.hasView && condition) {
-      this.viewContainerRef.clear();
-      this.hasView = false;
-    }
-  }
-
-  constructor(
-    private templateRef: TemplateRef<any>,
-    private viewContainerRef: ViewContainerRef
-  ) {}
+  constructor() {}
 }
--- src/app/tpl-outlet/tpl-outlet.component.ts
+++ src/app/tpl-outlet/tpl-outlet.component.ts
@@ -12,4 +12,6 @@ export class TplOutletComponent {
     $implicit: 'default-value',
     name: 'vgri',
   };
+
+  show = false;
 }
